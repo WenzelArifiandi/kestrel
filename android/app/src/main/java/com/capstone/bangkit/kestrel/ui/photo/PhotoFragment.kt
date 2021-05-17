ee48261f669397ca8f932d2083eba809b14de064
Fozimat <fozimata@gmail.com>
2021-05-17 17:33:48 +0700

    Add view for capture image

--- android/app/src/main/java/com/capstone/bangkit/kestrel/ui/photo/PhotoFragment.kt
+++ android/app/src/main/java/com/capstone/bangkit/kestrel/ui/photo/PhotoFragment.kt
@@ -4,28 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.bangkit.kestrel.R
 
 class PhotoFragment : Fragment() {
-
-    private lateinit var photoViewModel: PhotoViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        photoViewModel =
-            ViewModelProvider(this).get(PhotoViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_photo, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        photoViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_photo, container, false)
     }
 }
