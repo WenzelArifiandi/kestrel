11c8b8601f584beb6b1b93acd8cbe8fba5b8a913
Fozimat <fozimata@gmail.com>
2021-05-17 14:27:30 +0700

    Add Splash Screen

--- android/app/src/main/java/com/capstone/bangkit/kestrel/ui/MainActivity.kt
+++ android/app/src/main/java/com/capstone/bangkit/kestrel/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.capstone.bangkit.kestrel
+package com.capstone.bangkit.kestrel.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.capstone.bangkit.kestrel.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_photo, R.id.navigation_history))
+            R.id.navigation_home, R.id.navigation_photo, R.id.navigation_history
+        ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ android/app/src/main/java/com/capstone/bangkit/kestrel/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.capstone.bangkit.kestrel.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.bangkit.kestrel.databinding.ActivitySplashBinding
+import com.capstone.bangkit.kestrel.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 1500)
+    }
+}
