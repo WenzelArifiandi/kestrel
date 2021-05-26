11a9e5ba1b45ef017b99918687c5da9e29d41aae
Fozimat <fozimata@gmail.com>
2021-05-26 10:11:26 +0700

    Add Capture Image

--- android/app/src/main/java/com/capstone/bangkit/kestrel/ui/photo/PhotoFragment.kt
+++ android/app/src/main/java/com/capstone/bangkit/kestrel/ui/photo/PhotoFragment.kt
@@ -1,20 +1,117 @@
 package com.capstone.bangkit.kestrel.ui.photo
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.capstone.bangkit.kestrel.R
+import com.capstone.bangkit.kestrel.databinding.FragmentPhotoBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PhotoFragment : Fragment() {
+
+    private var _binding: FragmentPhotoBinding? = null
+    private val binding get() = _binding!!
+
+    private val REQUEST_PERMISSION = 100
+    private val REQUEST_CAPTURE_IMAGE = 1
+    private lateinit var currentPhotoPath: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_photo, container, false)
+    ): View {
+        _binding = FragmentPhotoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCapture.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            val packageManager = activity?.packageManager
+            if (packageManager != null) {
+                intent.resolveActivity(packageManager)?.also {
+                    val photoFile: File? = try {
+                        createCapturedPhoto()
+                    } catch (ex: IOException) {
+                        null
+                    }
+                    photoFile?.also {
+                        val photoURI = FileProvider.getUriForFile(
+                            requireActivity(),
+                            "com.capstone.bangkit.kestrel.fileprovider",
+                            it
+                        )
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                        startActivityForResult(intent, REQUEST_CAPTURE_IMAGE)
+                    }
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createCapturedPhoto(): File {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd-HHmmss", Locale.ENGLISH).format(Date())
+        val storageDir = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("IMG_${timestamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSION
+            )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkCameraPermission()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            if (requestCode == REQUEST_CAPTURE_IMAGE) {
+                val uri = Uri.parse(currentPhotoPath)
+                binding.photo.setImageURI(uri)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
