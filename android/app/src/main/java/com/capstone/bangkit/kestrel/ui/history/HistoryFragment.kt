41229ea5b94edcfc532e4cc14cfce2477705f412
Fozimat <fozimata@gmail.com>
2021-06-02 21:05:42 +0700

    Update state for empty data history

--- android/app/src/main/java/com/capstone/bangkit/kestrel/ui/history/HistoryFragment.kt
+++ android/app/src/main/java/com/capstone/bangkit/kestrel/ui/history/HistoryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.capstone.bangkit.kestrel.databinding.FragmentHistoryBinding
@@ -17,6 +18,7 @@ class HistoryFragment : Fragment() {
     private var _binding: FragmentHistoryBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var adapter: HistoryAdapter
     private val list: ArrayList<History> = arrayListOf()
 
     override fun onCreateView(
@@ -33,29 +35,32 @@ class HistoryFragment : Fragment() {
         loadData()
     }
 
-
     private fun showRecyclerView() {
-        binding.rvHistory.setHasFixedSize(true)
-        binding.rvHistory.layoutManager = LinearLayoutManager(context)
-        val adapter = HistoryAdapter(list)
-        binding.rvHistory.adapter = adapter
-        adapter.notifyDataSetChanged()
+
+        if(list.isEmpty()) {
+            binding.layoutEmpty.isVisible = true
+        } else {
+            binding.layoutEmpty.isVisible = false
+            binding.rvHistory.setHasFixedSize(true)
+            binding.rvHistory.layoutManager = LinearLayoutManager(context)
+            adapter = HistoryAdapter(list)
+            binding.rvHistory.adapter = adapter
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun loadData() {
         db.collection("histories").orderBy("date", Query.Direction.DESCENDING)
-            .addSnapshotListener(object : EventListener<QuerySnapshot?> {
-                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
-                    if (value != null) {
-                        for (doc in value.documentChanges) {
-                            if (doc.type == DocumentChange.Type.ADDED) {
-                                list.add(doc.document.toObject(History::class.java))
-                            }
+            .addSnapshotListener { value, _ ->
+                if (value != null) {
+                    for (doc in value.documentChanges) {
+                        if (doc.type == DocumentChange.Type.ADDED) {
+                            list.add(doc.document.toObject(History::class.java))
                         }
-                        showRecyclerView()
                     }
+                    showRecyclerView()
                 }
-            })
+            }
     }
 
 }
